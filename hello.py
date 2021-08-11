ea1f8b92c372de23d5bdb525dac4a8d49f932ec3
Pratima Maiti <pratima.maiti@nagarro.com>
2021-08-11 11:29:09 +0530

    Revert "Git revert"
    This reverts commit a8e9f63398e3efd1a5224b6804b359389b4df4ae.

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ hello=input("Are you new on git")
 if hello == "yes":
     print("Welcome to git")
     print("Git is quite interesting")
-    print("Now undo committed changes")
+
 else:
     print("Practice git commands well")

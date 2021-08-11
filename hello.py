d3ba700ea02f00f0ddc38ee53176358176770c92
Pratima Maiti <pratima.maiti@nagarro.com>
2021-08-11 11:25:59 +0530

    third version

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ hello=input("Are you new on git")
 if hello == "yes":
     print("Welcome to git")
     print("Git is quite interesting")
-    print("This message is to be deleted by git checkout -- hello.py")
+
 else:
     print("Practice git commands well")

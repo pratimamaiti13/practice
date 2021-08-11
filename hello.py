63734932fea722a6c0bb047acd5340a0a7e1c434
Pratima Maiti <pratima.maiti@nagarro.com>
2021-08-11 11:33:50 +0530

    dummy 2

--- hello.py
+++ hello.py
@@ -3,5 +3,6 @@ if hello == "yes":
     print("Welcome to git")
     print("Git is quite interesting")
     print("This is a dummy text for using git reset command")
+    print("This is a dummy text for using git reset command")
 else:
     print("Practice git commands well")
